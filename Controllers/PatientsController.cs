748f394983432b40ada93ff3113dc8ffaf67a76c
unknown <berstein.vivi66@gmail.com>
2024-12-12 09:47:03 +0100

    arrangement des fichiers

--- Controllers/PatientsController.cs
+++ Controllers/PatientsController.cs
@@ -44,8 +44,6 @@ namespace MedicalAppointmentsApp.Controllers
         }
 
         // POST: Patients/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("PatientId,FirstName,LastName,BirthDate,Description")] Patient patient)
@@ -76,8 +74,6 @@ namespace MedicalAppointmentsApp.Controllers
         }
 
         // POST: Patients/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("PatientId,FirstName,LastName,BirthDate,Description")] Patient patient)
